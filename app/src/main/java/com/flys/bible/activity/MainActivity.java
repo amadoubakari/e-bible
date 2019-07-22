3ced6c9b2533319a8321fea5e2e91bc7e2c64a82
AMADOU BAKARI <amadoubakari1992@gmail.com>
2019-07-22 06:15:39 -0700

    Feature: hme
    first screen showing daily verset

--- app/src/main/java/com/flys/bible/activity/MainActivity.java
+++ app/src/main/java/com/flys/bible/activity/MainActivity.java
@@ -13,9 +13,11 @@ import com.flys.bible.dao.service.IDao;
 import org.androidannotations.annotations.Bean;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
+
 import com.flys.bible.R;
 import com.flys.bible.fragments.behavior.DummyFragment_;
 import com.flys.bible.fragments.behavior.GeneralSettingsFragment_;
+import com.flys.bible.fragments.behavior.HomeFragment_;
 import com.flys.bible.fragments.behavior.MainFragment_;
 import com.flys.bible.fragments.behavior.SettingsFragment_;
 
@@ -24,82 +26,82 @@ import com.flys.bible.fragments.behavior.SettingsFragment_;
 @OptionsMenu(R.menu.menu_vide)
 public class MainActivity extends AbstractActivity {
 
-  // couche [DAO]
-  @Bean(Dao.class)
-  protected IDao dao;
-  // session
-  private Session session;
-
-  // méthodes classe parent -----------------------
-  @Override
-  protected void onCreateActivity() {
-    // log
-    if (IS_DEBUG_ENABLED) {
-      Log.d(className, "onCreateActivity");
-    }
+    // couche [DAO]
+    @Bean(Dao.class)
+    protected IDao dao;
     // session
-    this.session = (Session) super.session;
-    // todo : on continue les initialisations commencées par la classe parent
-  }
+    private Session session;
 
-  @Override
-  protected IDao getDao() {
-    return dao;
-  }
+    // méthodes classe parent -----------------------
+    @Override
+    protected void onCreateActivity() {
+        // log
+        if (IS_DEBUG_ENABLED) {
+            Log.d(className, "onCreateActivity");
+        }
+        // session
+        this.session = (Session) super.session;
+        // todo : on continue les initialisations commencées par la classe parent
+    }
 
-  @Override
-  protected AbstractFragment[] getFragments() {
-    // todo : définir les fragments ici
-    return new AbstractFragment[]{new MainFragment_(),new DummyFragment_(),new SettingsFragment_(),new GeneralSettingsFragment_()};
-  }
+    @Override
+    protected IDao getDao() {
+        return dao;
+    }
+
+    @Override
+    protected AbstractFragment[] getFragments() {
+        // todo : définir les fragments ici
+        return new AbstractFragment[]{new MainFragment_(), new DummyFragment_(), new SettingsFragment_(), new GeneralSettingsFragment_(),new HomeFragment_()};
+    }
 
 
-  @Override
-  protected CharSequence getFragmentTitle(int position) {
-    // todo : définir les titres des fragments ici
-    return null;
-  }
+    @Override
+    protected CharSequence getFragmentTitle(int position) {
+        // todo : définir les titres des fragments ici
+        return null;
+    }
 
-  @Override
-  protected void navigateOnTabSelected(int position) {
-    // todo : navigation par onglets - définir la vue à afficher lorsque l'onglet n° [position] est sélectionné
-  }
+    @Override
+    protected void navigateOnTabSelected(int position) {
+        // todo : navigation par onglets - définir la vue à afficher lorsque l'onglet n° [position] est sélectionné
+    }
 
-  @Override
-  protected int getFirstView() {
-    // todo : définir le n° de la première vue (fragment) à afficher
-    return 0;
-  }
+    @Override
+    protected int getFirstView() {
+        // todo : définir le n° de la première vue (fragment) à afficher
+        return 0;
+    }
 
-  @Override
-  protected boolean isCollapse() {
-    return false;
-  }
+    @Override
+    protected boolean isCollapse() {
+        return false;
+    }
 
-  @Override
-  public boolean onSupportNavigateUp() {
-    return true;
-  }
+    @Override
+    public boolean onSupportNavigateUp() {
+        return true;
+    }
 
-  //Navigation
-  @Override
-  public void onBackPressed() {
-    navigateToView(session.getPreviousView(), ISession.Action.SUBMIT);
-  }
+    //Navigation
+    @Override
+    public void onBackPressed() {
+        navigateToView(session.getPreviousView(), ISession.Action.SUBMIT);
+    }
 
-  @Override
-  public void settings() {
-    navigateToView(2, ISession.Action.SUBMIT);
-  }
+    @Override
+    public void settings() {
+        navigateToView(2, ISession.Action.SUBMIT);
+    }
 
-  @Override
-  public void home() {
-    Toast.makeText(this,"En cours ...",Toast.LENGTH_LONG).show();
-  }
+    @Override
+    public void home() {
+        navigateToView(4, ISession.Action.SUBMIT);
+    }
 
-  @Override
-  public void bible()  {
-    navigateToView(0, ISession.Action.SUBMIT);
-  }
+    @Override
+    public void bible() {
+        navigateToView(0, ISession.Action.SUBMIT);
+    }
 }
 
--- app/src/main/java/com/flys/bible/fragments/behavior/HomeFragment.java
+++ app/src/main/java/com/flys/bible/fragments/behavior/HomeFragment.java
@@ -1,8 +1,14 @@
 package com.flys.bible.fragments.behavior;
 
+import com.flys.bible.R;
 import com.flys.bible.architecture.core.AbstractFragment;
 import com.flys.bible.architecture.custom.CoreState;
 
+import org.androidannotations.annotations.EFragment;
+import org.androidannotations.annotations.OptionsMenu;
+
+@OptionsMenu(R.menu.menu_main)
+@EFragment(R.layout.home_item)
 public class HomeFragment extends AbstractFragment {
     @Override
     public CoreState saveFragment() {
